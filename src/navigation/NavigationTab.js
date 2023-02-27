992162625259a3ea2b9e8fc87f11fdb4b1e6bb51
edward vega <edward.vega@wiedii.co>
2023-02-26 20:37:00 -0500

    add first commit

---
+++ src/navigation/NavigationTab.js
@@ -0,0 +1,50 @@
+import React from "react";
+import { Image } from "react-native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import Ionicons from "react-native-vector-icons/Ionicons";
+import Favorite from "../screen/Favorite";
+import Account from "../screen/Account";
+import PokeDesk from "../screen/PokeDesk";
+
+const Tab = createBottomTabNavigator();
+
+export default function NavigationTab() {
+  return (
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ focused, color, size }) => {
+          let iconName;
+          switch (route.name) {
+            case "Favorite":
+              iconName = focused ? "heart-sharp" : "heart-outline";
+              break;
+            case "Account":
+              iconName = focused ? "person" : "person-outline";
+              break;
+            case "PokeDesk":
+              return (
+                <Image
+                  style={{ width: 70, height: 70, top: -15 }}
+                  source={require("../../assets/pokeball.png")}
+                />
+              );
+            default:
+              break;
+          }
+          return <Ionicons name={iconName} size={size} color={color} />;
+        },
+        tabBarActiveTintColor: "dodgerblue",
+        tabBarInactiveTintColor: "gray",
+      })}
+    >
+      <Tab.Screen name="Favorite" component={Favorite} />
+      <Tab.Screen
+        name="PokeDesk"
+        component={PokeDesk}
+        options={{ tabBarLabel: "" }}
+      />
+
+      <Tab.Screen name="Account" component={Account} />
+    </Tab.Navigator>
+  );
+}
---
+++ src/screen/Account.js
@@ -0,0 +1,10 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+export default function Account() {
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text>Account!</Text>
+    </View>
+  );
+}
